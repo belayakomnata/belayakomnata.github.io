f41a52a16f3111eb4f87578f169d71e81c3606b5
Emil Yangirov <emil.yangirov@list.ru>
2019-10-11 08:01:06 +0500

    fix: Небольшие исправления

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -41,8 +41,13 @@ gulp.task('copy:rootfiles', () => gulp
   .pipe(gulp.dest(config.dest.root))
 );
 
-const build = gulp => gulp.series('copy:img', 'copy:fonts', 'copy:data');
-const watch = gulp => () => gulp.watch(config.src.img + '/*', gulp.parallel('copy:img', 'copy:fonts'));
+gulp.task('copy:js', () => gulp
+  .src(config.src.js + '/*.*')
+  .pipe(gulp.dest(config.dest.js))
+);
+
+const build = gulp => gulp.series('copy:img', 'copy:fonts', 'copy:data', 'copy:js');
+const watch = gulp => () => gulp.watch(config.src.img + '/*', gulp.parallel('copy:img', 'copy:fonts', 'copy:js'));
 
 module.exports.build = build;
 module.exports.watch = watch;

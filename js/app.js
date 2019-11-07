eb573421010602d3c31c7d5ca4dafb2338485b09
Emil Yangirov <emil.yangirov@list.ru>
2019-11-07 21:09:08 +0500

    Update

--- js/app.js
+++ js/app.js
@@ -2,22 +2,25 @@ if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js');
 }
 
-let deffer;
-const pwaBtn = document.getElementById("pwa-btn");
+let deferredPrompt;
+const addBtn = document.getElementById("pwa-btn");
+addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-    deffer = e;
-  
-    pwaBtn.addEventListener('click', (ec) => {
-        deffer.prompt();
-        deffer.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the prompt');
-            } else {
-                console.log('User dismissed the prompt');
-            }
-            deffer = null;
-        });
-    });
+  e.preventDefault();
+  deferredPrompt = e;
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (ec) => {
+    addBtn.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
 });
